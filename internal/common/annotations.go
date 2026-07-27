bea6f17dad0ba766f207ac9f61ba3b89675a28ee
Ezz Ashraf <ezzops@github.com>
2026-07-27 10:36:04 +0000

    gofmt annotations.go

--- internal/common/annotations.go
+++ internal/common/annotations.go
@@ -1,7 +1,7 @@
 package common
 
 const (
-	AnnotationEnabled    = "idle-scale.nous.io/enabled"
-	AnnotationIdleTimeout = "idle-scale.nous.io/idle-timeout"
+	AnnotationEnabled      = "idle-scale.nous.io/enabled"
+	AnnotationIdleTimeout  = "idle-scale.nous.io/idle-timeout"
 	AnnotationStartupGrace = "idle-scale.nous.io/startup-grace"
 )
